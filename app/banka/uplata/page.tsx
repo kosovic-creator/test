136b179d02ed7303a572cc4e512cdd2d54b1c4f7
kosovic-creator <drasko.kosovic@icloud.com>
2025-10-21 21:27:06 +0200

    Uplata2

--- app/banka/uplata/page.tsx
+++ app/banka/uplata/page.tsx
@@ -1,41 +1,64 @@
+//
 'use client'
 import React from 'react'
 
-const UplataPage = () => {
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        const formData = new FormData(event.target as HTMLFormElement);
-        const to = formData.get('to');
-        const amount = formData.get('amount');
+const Uplata = () => {
+    // const [from, setFrom] = React.useState('');
+    const [to, setTo] = React.useState('');
+    const [amount, setAmount] = React.useState('');
+    const [result, setResult] = React.useState<string | null>(null);
 
-        const response = await fetch('/api/banka/uplata', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ to, amount }),
-        });
-
-        const data = await response.json();
-        console.log(data);
+    const handleUplata = async () => {
+      const response = await fetch('/api/banka/uplata', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          to,
+          amount,
+        }),
+      });
+      const data = await response.json();
+      setResult(data.message);
     };
-
   return (
     <div>
       <h1>Uplata</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleUplata}>
+        {/* <div>
+          <label htmlFor="from">Pošiljalac:</label>
+          <input
+            type="email"
+            id="from"
+            value={from}
+            onChange={e => setFrom(e.target.value)}
+            required
+          />
+        </div> */}
         <div>
           <label htmlFor="to">Primalac:</label>
-          <input type="email" id="to" name="to" required />
+          <input
+            type="email"
+            id="to"
+            value={to}
+            onChange={e => setTo(e.target.value)}
+            required
+          />
         </div>
         <div>
           <label htmlFor="amount">Iznos:</label>
-          <input type="number" id="amount" name="amount" required />
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={e => setAmount(e.target.value)}
+            required
+          />
         </div>
         <button type="submit">Uplati</button>
       </form>
+      {result && <p>{result}</p>}
     </div>
   )
 }
 
-export default UplataPage
+export default Uplata
