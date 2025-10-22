bd9b5da7ea5060ae9c4e8b46aa6e58e3a1a80d0e
kosovic-creator <drasko.kosovic@icloud.com>
2025-10-22 08:32:52 +0200

    aa

--- app/getpost/get/page.tsx
+++ app/getpost/get/page.tsx
@@ -7,17 +7,17 @@ import React, { useEffect } from 'react'
 
 const OdgovorPage = () => {
 
-const [data, setData] = React.useState<{ odgovor?: any }>({});
+  const [data, setData] = React.useState<{ odgovor?: any }>({});
   useEffect(() => {
     const odgovor = async () => {
       try {
-         await fetch('/api/getpost/get', {
+        await fetch('/api/getpost/get', {
           method: 'GET',
         }).then(res => res.json())
-        .then(data => {
-          console.log('API odgovor data:', data)
-          setData({ odgovor: data })
-        })
+          .then(data => {
+            console.log('API odgovor data:', data)
+            setData({ odgovor: data })
+          })
       } catch (error) {
         console.error('Error fetching odgovor:', error)
       }
@@ -28,10 +28,10 @@ const [data, setData] = React.useState<{ odgovor?: any }>({});
 
   return (
     <div>
-<p>Odgovor stranica</p>
-<a >Pozovi API odgovor</a>
-{data.odgovor && <p>{data.odgovor.message}</p>}
-<Greeting name="Marko" />
+      <p>Odgovor stranica</p>
+      <a >Pozovi API odgovor</a>
+      {data.odgovor && <p>{data.odgovor.message}</p>}
+      <Greeting name="Marko" />
     </div>
   )
 }
