0fe91d167a7f15e252e3f2f163dea2104762db2f
kosovic-creator <drasko.kosovic@icloud.com>
2025-10-25 09:11:16 +0200

    session na page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Greeting from "@/components/Greeting";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client';
+import { useState } from "react";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+
+   const { data: session, status } = useSession()
+   const ime = session?.user?.name;
   return (
     <>
-     <Greeting name="Marko" />
+     Zdravo: {ime}
     </>
   );
 }
