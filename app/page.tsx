5d71b4de49da1d98f763c670703e9898fa4a20e0
kosovic-creator <drasko.kosovic@icloud.com>
2025-10-27 20:24:47 +0100

    data umjesto eventListner Provider korpa context

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 'use client';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 
 export default function Home() {
 
    const { data: session, status } = useSession()
    const ime = session?.user?.name;
+  useEffect(() => {
+  const handleResize = () => {
+    console.log('Veličina prozora je promijenjena!');
+  };
+  window.addEventListener('resize', handleResize);
+  return () => {
+    window.removeEventListener('resize', handleResize);
+  };
+}, []);
   return (
     <>
      Zdravo: {ime}
+
     </>
   );
 }
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,11 +1,12 @@
 "use client"
 
-import React, { ReactNode, useEffect, useContext } from 'react';
+import React, { ReactNode, useEffect, useContext, useState } from 'react';
 import { MyProvider } from './MyContext';
 import ToastProvider from './ToastProvider';
 import { SessionProvider, useSession } from 'next-auth/react';
 import Navbar from './Navbar';
 import MyContext from './MyContext';
+
 
 type StavkaKorpe = {
   id: number;
@@ -19,6 +20,7 @@ type StavkaKorpe = {
 function KorpaContextUpdater() {
   const { data: session } = useSession();
   const context = useContext(MyContext);
+  const [data, setData] = useState<StavkaKorpe[]>([]);
 
   useEffect(() => {
     if (!session || !context) return;  // Ako nema sesije ili konteksta, ne radi ništa
@@ -28,6 +30,7 @@ function KorpaContextUpdater() {
       fetch(`/api/prodavnica/korpa?korisnikId=${session.user.id}`)
         .then(res => res.json())
         .then((data: StavkaKorpe[]) => {
+          setData(data);
           // Zbroj količina svih stavki u korpi
           const totalKolicina = data.reduce((sum, item) => sum + item.kolicina, 0);
           // Postavi brojka u kontekst da se može koristiti u drugim dijelovima aplikacije
@@ -39,14 +42,14 @@ function KorpaContextUpdater() {
 
     // Dodaj listener na globalni window koji će slušati 'korpa-updated' događaj
     // svaki put kad se on dogodi poziva fetchKorpa da osveži stanje korpe
-    window.addEventListener('korpa-updated', fetchKorpa);
+    // window.addEventListener('korpa-updated', fetchKorpa);
 
-    // Cleanup funkcija koja se poziva prilikom unmountovanja ili promjene dependencija
-    // - uklanja event listener da se ne bi acumirale višestruke funkcije
-    return () => {
-      window.removeEventListener('korpa-updated', fetchKorpa);
-    };
-  }, [session, context]);
+    // // Cleanup funkcija koja se poziva prilikom unmountovanja ili promjene dependencija
+    // // - uklanja event listener da se ne bi acumirale višestruke funkcije
+    // return () => {
+    //   window.removeEventListener('korpa-updated', fetchKorpa);
+    // };
+  }, [session, context, data]);
 
   return null;
 }
