f80cb0b1ee6bd549a10f0b8dcf48e922479c1fa1
kosovic-creator <drasko.kosovic@icloud.com>
2025-11-01 18:07:23 +0100

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          <Navbar />
+          
           {children}
         </Providers>
 
