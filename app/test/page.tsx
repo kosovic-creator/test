59c5552c988daa518e7f981ba3f2cf2485fa4ae1
kosovic-creator <drasko.kosovic@icloud.com>
2025-11-01 08:59:27 +0100

    nav bar

---
+++ app/test/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      {console.log("Banka page renderovan")}
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default page
